60ee690560e5ae57c554a88ec7a953ba6a9f06fc
mdmehedihasan212 <mdmehedihasan384@gmail.com>
2022-05-12 10:27:06 +0600

    add nothing change only for deploy issue

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const jwt = require('jsonwebtoken')
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 5000;
@@ -12,6 +13,30 @@ app.use(express.json())
 app.get('/', (req, res) => {
     res.send('Doctors Portal Client Site!')
 })
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@doctorsprotal.dzwyi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+
+    try {
+        await client.connect();
+        const serviceCollection = client.db('doctorsPortal').collection('services')
+
+        app.get('/services', async (req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
+
 
 app.listen(port, () => {
     console.log(`Doctors Portal Server Site listening on port ${port}`)
