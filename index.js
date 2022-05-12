3aabbf58fa1c2fa49e6fa101c307545ee5681cda
mdmehedihasan212 <mdmehedihasan384@gmail.com>
2022-05-12 10:33:02 +0600

    deploy issue

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(cors())
 app.use(express.json())
 
 app.get('/', (req, res) => {
-    res.send('Doctors Portal Client Site!')
+    res.send('Doctors Portal Client Site Deploy Heroku!')
 })
 
 
