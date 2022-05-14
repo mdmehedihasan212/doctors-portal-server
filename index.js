70c7fdd470cb38363366f010d2a1c11e0db37776
mdmehedihasan212 <mdmehedihasan384@gmail.com>
2022-05-15 03:27:13 +0600

    add query post app

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ app.get('/', (req, res) => {
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@doctorsprotal.dzwyi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
@@ -23,12 +24,24 @@ async function run() {
     try {
         await client.connect();
         const serviceCollection = client.db('doctorsPortal').collection('services')
+        const bookingCollection = client.db('doctorsPortal').collection('bookings')
 
         app.get('/services', async (req, res) => {
             const query = {};
             const cursor = serviceCollection.find(query);
             const services = await cursor.toArray();
             res.send(services);
+        })
+
+        app.post('/booking', async (req, res) => {
+            const booking = req.body;
+            const query = { treatment: booking.treatment, date: booking.date, patient: booking.patient }
+            const exists = await bookingCollection.findOne(query)
+            if (exists) {
+                return res.send({ success: false, booking: exists })
+            }
+            const result = await bookingCollection.insertOne(booking);
+            return res.send({ success: true, booking })
         })
     }
     finally {
